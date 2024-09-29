76ce41488326cd2c3e60b6ecdc07f2732447ad1f
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-09-29 23:56:43 +0500

    Free Look camera added in fps.
    Now player can look in mouse moving direction and can move.

--- Assets/GamesBySaad/CharacterController/Scripts/PlayerController.cs
+++ Assets/GamesBySaad/CharacterController/Scripts/PlayerController.cs
@@ -6,14 +6,24 @@ namespace FablockGaming.FinalCharacterController
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private CharacterController CharacterController;
+        [Header("Componenets")]
         [SerializeField] private Camera PlayerCamera;
+        [SerializeField] private CharacterController CharacterController;
 
+        [Header("Base Movement")]
         public float RunAcceleration = 0.25f;
         public float RunSpeed = 4f;
         public float Drag = 0.1f;
 
+        [Header("Camera Settings")]
+        public float LookSenseH = 0.1f;
+        public float LookSenseV = 0.1f;
+        public float LookLimitV = 89f;
+
         private PlayerLocomotionInput _playerLocomotionInput;
+
+        private Vector2 _cameraRotation = Vector2.zero;
+        private Vector2 _playerTargetRotation = Vector2.zero;
 
         private void Awake()
         {
@@ -33,6 +43,17 @@ namespace FablockGaming.FinalCharacterController
             _newVelocity = Vector3.ClampMagnitude(_newVelocity, RunSpeed);
 
             CharacterController.Move(_newVelocity*Time.deltaTime);
+        }
+        private void LateUpdate()
+        {
+            _cameraRotation.x += LookSenseH * _playerLocomotionInput.LookInput.x;
+            _cameraRotation.y = Mathf.Clamp(_cameraRotation.y - LookSenseV * _playerLocomotionInput.LookInput.y, -LookLimitV, LookLimitV);
+
+            _playerTargetRotation.x += transform.eulerAngles.x + LookSenseH * _playerLocomotionInput.LookInput.x;
+            transform.rotation = Quaternion.Euler(0f, _playerTargetRotation.x, 0f);
+
+            PlayerCamera.transform.rotation = Quaternion.Euler(_cameraRotation.y, _cameraRotation.x, 0f);
+
         }
     }
 }
