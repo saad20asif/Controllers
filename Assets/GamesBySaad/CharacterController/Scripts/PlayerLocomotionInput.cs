cf791c133d30f6054c6745015aa84b4db96b5f34
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-09-29 21:47:26 +0500

    Player Input script!

---
+++ Assets/GamesBySaad/CharacterController/Scripts/PlayerLocomotionInput.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using FablockGaming.FinalCharacterController.Input;
+using UnityEngine.InputSystem;
+using Unity.VisualScripting;
+
+namespace FablockGaming.FinalCharacterController
+{
+    public class PlayerLocomotionInput : MonoBehaviour, PlayerControls.IPlayerLocomotionMapActions
+    {
+        public PlayerControls PlayerControls { get; private set; }
+        public Vector2 MovementInput { get; private set; }
+        private void OnEnable()
+        {
+            PlayerControls = new PlayerControls();
+            PlayerControls.Enable();
+
+            PlayerControls.PlayerLocomotionMap.Enable();
+            PlayerControls.PlayerLocomotionMap.SetCallbacks(this);
+        }
+        private void OnDisable()
+        {
+            PlayerControls.PlayerLocomotionMap.Disable();
+            PlayerControls.PlayerLocomotionMap.RemoveCallbacks(this);
+        }
+
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            MovementInput = context.ReadValue<Vector2>();
+            print(MovementInput);
+        }
+    }
+}
+
