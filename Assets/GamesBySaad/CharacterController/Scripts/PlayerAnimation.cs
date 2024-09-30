080e007b44454606fa3729e55b0b014096b6a673
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-09-30 23:38:59 +0500

    Blend value lerped for smooth transition!

--- Assets/GamesBySaad/CharacterController/Scripts/PlayerAnimation.cs
+++ Assets/GamesBySaad/CharacterController/Scripts/PlayerAnimation.cs
@@ -8,10 +8,13 @@ namespace FablockGaming.FinalCharacterController
     public class PlayerAnimation : MonoBehaviour
     {
         [SerializeField] private Animator Animator;
+        [SerializeField] private float LocomotionBlendSpeed = 0.2f;
         private PlayerLocomotionInput _playerLocomotionInput;
 
         private static int inputXHash = Animator.StringToHash("inputX");
         private static int inputYHash = Animator.StringToHash("inputY");
+
+        private Vector3 _currentBlendInput = Vector3.zero;
 
         private void Awake()
         {
@@ -26,9 +29,10 @@ namespace FablockGaming.FinalCharacterController
         private void UpdateAnimationState()
         {
             Vector2 _inputTarget = _playerLocomotionInput.MovementInput;
+            _currentBlendInput = Vector3.Lerp(_currentBlendInput, _inputTarget, LocomotionBlendSpeed * Time.deltaTime);
             print("_inputTarget : " + _inputTarget);
-            Animator.SetFloat(inputXHash, _inputTarget.x);
-            Animator.SetFloat(inputYHash, _inputTarget.y);
+            Animator.SetFloat(inputXHash, _currentBlendInput.x);
+            Animator.SetFloat(inputYHash, _currentBlendInput.y);
         }
     }
 }
