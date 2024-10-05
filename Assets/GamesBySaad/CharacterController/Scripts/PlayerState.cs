280c945c53c5e691345861e60458ea89430d1102
saad20asif <48903984+saad20asif@users.noreply.github.com>
2024-10-05 13:36:35 +0500

    PlayerState script!

--- Assets/GamesBySaad/CharacterController/Scripts/PlayerState.cs
+++ Assets/GamesBySaad/CharacterController/Scripts/PlayerState.cs
@@ -2,18 +2,24 @@ using UnityEngine;
 
 namespace FablockGaming.FinalCharacterController
 {
+    public enum PlayerMovementState
+    {
+        Idling = 0,
+        Walking = 1,
+        Running = 2,
+        Sprinting = 3,
+        Jumping = 4,
+        Falling = 5,
+        Strafing = 6
+    }
     public class PlayerState : MonoBehaviour
     {
         [field:SerializeField] public PlayerMovementState CurrentPlayerMovementState {  get; private set; } = PlayerMovementState.Idling;
-        public enum PlayerMovementState
+
+        public void SetPlayerMovement(PlayerMovementState playerMovementState)
         {
-            Idling = 0,
-            Walking = 1,
-            Running = 2,
-            Sprinting = 3,
-            Jumping = 4,
-            Falling = 5,
-            Strafing = 6
+            CurrentPlayerMovementState = playerMovementState;
         }
+        
     }
 }
